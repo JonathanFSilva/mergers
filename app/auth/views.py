c34ee9eb0888f611636c82bcae496e896c4fde5d
Wadson Garbes <wadson.goncalves@fatec.sp.gov.br>
2017-05-15 12:02:39 +0000

    cria as views para as rotas /login, /cadastrar e /logout, além de validar os formulários e incluí-los no banco de dados

---
+++ app/auth/views.py
@@ -0,0 +1,74 @@
+# app/auth/views.py
+
+from flask import flash, redirect, render_template, url_for
+from flask_login import login_required, login_user, logout_user
+
+from . import auth
+from forms import LoginForm, RegistrationForm
+from .. import db
+from ..models import Employee
+
+@auth.route('/cadastrar', methods=['GET', 'POST'])
+def cadastrar():
+    """
+    Cuida da rota /cadastrar
+    Adiciona uma empresa ao banco de dados
+    """
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        employee = Employee(email=form.email.data,
+                            username=form.username.data,
+                            first_name=form.first_name.data,
+                            last_name=form.last_name.data,
+                            password=form.password.data)
+
+        # add employee to the database
+        db.session.add(employee)
+        db.session.commit()
+        flash('Cadastro feito com sucesso!')
+
+        # redirect to the login page
+        return redirect(url_for('auth.login'))
+
+    # load registration template
+    return render_template('auth/cadastrar.html', form=form, title='Cadastrar')
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    """
+    Handle requests to the /login route
+    Log an employee in through the login form
+    """
+    form = LoginForm()
+    if form.validate_on_submit():
+
+        # check whether employee exists in the database and whether
+        # the password entered matches the password in the database
+        employee = Employee.query.filter_by(email=form.email.data).first()
+        if employee is not None and employee.verify_password(
+                form.password.data):
+            # log employee in
+            login_user(employee)
+
+            # redirect to the dashboard page after login
+            return redirect(url_for('home.dashboard'))
+
+        # when login details are incorrect
+        else:
+            flash('Email ou senha inválidos')
+
+    # load login template
+    return render_template('auth/login.html', form=form, title='Login')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    """
+    Handle requests to the /logout route
+    Log an employee out through the logout link
+    """
+    logout_user()
+    flash('Sessão encerrada com sucesso!')
+
+    # redirect to the login page
+    return redirect(url_for('auth.login'))
