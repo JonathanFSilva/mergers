500527ccdafd611308cb2c2aa1312fb9acf34157
Wadson Garbes <wadson.goncalves@fatec.sp.gov.br>
2017-05-15 03:33:03 +0000

    conserta typo bluprint

--- app/__init__.py
+++ app/__init__.py
@@ -31,9 +31,9 @@ def create_app(config_name):
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_bluprint)
+    app.register_blueprint(auth_blueprint)
 
-    from .home import home as home_bluprint
+    from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
 
     return app
