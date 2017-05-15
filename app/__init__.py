b098439acccc7893c0f004f36f82880cad332e51
Wadson Garbes <wadson.goncalves@fatec.sp.gov.br>
2017-05-15 12:06:41 +0000

    instancia o app com o flask_bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 # importações locais
 from config import app_config
@@ -25,6 +26,8 @@ def create_app(config_name):
     login_manager.login_view = "auth.login"
 
     migrate = Migrate(app, db)
+
+    Bootstrap(app)
     from app import models
 
     from .admin import admin as admin_blueprint
