cc802fc8635dedb2cea1f1ce6367553d753ea367
Wadson Garbes <wadson.goncalves@fatec.sp.gov.br>
2017-05-15 02:11:21 +0000

    importa a blueprint de cada projeto. Além disso, associa todas as views de admin ao prefixo /admin

--- app/__init__.py
+++ app/__init__.py
@@ -27,4 +27,13 @@ def create_app(config_name):
     migrate = Migrate(app, db)
     from app import models
 
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_bluprint)
+
+    from .home import home as home_bluprint
+    app.register_blueprint(home_blueprint)
+
     return app
