9bcef0f812c00eccb9a8868912932c70b782258e
Wadson <wadson.goncalves@fatec.sp.gov.br>
2017-05-29 22:36:10 +0000

    adiciona formulário do investimento

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
+
 from flask_wtf import FlaskForm 
 from wtforms import StringField, DateTimeField, FloatField, SubmitField
 from wtforms.validators import DataRequired
 
 class InvestimentoForm(FlaskForm):
-    """
+    '''
     Formulário para o admin adicionar ou editar um investimento (a.k.a participação)
-    """
+    '''
     tipo_invest = StringField('Tipo de investimento', validators=[DataRequired()])
     val_invest = FloatField('Valor do investimento', validators=[DataRequired()])
     data_invest = DateTimeField('Data e horário', format='%d-%m-%Y %H:M')
