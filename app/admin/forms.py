dbfd732a9decbc8de7a0d1f49547721b7e5174d9
Wadson <wadson.goncalves@fatec.sp.gov.br>
2017-06-07 02:03:47 +0000

    consultar PEP8 para melhor identação...

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -15,7 +15,8 @@ class InvestimentoForm(FlaskForm):
     '''
     tipo_invest = SelectField('Tipo de investimento', 
                     choices=[('Participação Societária', 'Participação Societária'), 
-                             ('Investimento em Sociedade anônima', 'Investimento em Sociedade anônima')])
+                             ('Investimento em Sociedade anônima', 'Investimento em Sociedade anônima')
+                            ])
     data_invest = StringField('Data e horário', validators=[DataRequired()], render_kw={"type": "date"})
     empresa_investidora = QuerySelectField(query_factory=lambda: Empresa.query.filter_by(is_admin=False),
                                     get_label='razao_social')
