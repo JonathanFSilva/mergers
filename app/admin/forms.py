663050374054f48cb9b0e4f84703d49e43ff6dd6
Wadson <wadson.goncalves@fatec.sp.gov.br>
2017-06-07 01:46:26 +0000

    Troca StringField por SelectField

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -3,7 +3,7 @@
 # app/admin/forms.py
 
 from flask_wtf import FlaskForm 
-from wtforms import StringField, DateField, FloatField, SubmitField
+from wtforms import StringField, DateField, FloatField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 
@@ -13,7 +13,9 @@ class InvestimentoForm(FlaskForm):
     '''
     Formulário para o admin adicionar ou editar um investimento (a.k.a participação)
     '''
-    tipo_invest = StringField('Tipo de investimento', validators=[DataRequired()])
+    tipo_invest = SelectField('Tipo de investimento', 
+                    choices=[('Participação Societária', 'Participação Societária'), 
+                             ('Investimento em Sociedade anônima', 'Investimento em Sociedade anônima')])
     data_invest = StringField('Data e horário', validators=[DataRequired()], render_kw={"type": "date"})
     empresa_investidora = QuerySelectField(query_factory=lambda: Empresa.query.filter_by(is_admin=False),
                                     get_label='razao_social')
