7808178e3a49e82cab15862a60177e85bdf3a31e
Wadson <wadson.goncalves@fatec.sp.gov.br>
2017-06-18 15:38:28 +0000

    adiciona as rotas de erro 403, 403 e 500

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 # app/__init__.py
 
 # importações de terceiros
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -38,5 +38,17 @@ def create_app(config_name):
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
+    
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template('erros/403.html', title="Proibido"), 403
+        
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('erros/404.html', title="Página não encontrada"), 404
+        
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('erros/500.html', title="Erro interno do servidor"), 500
 
     return app
