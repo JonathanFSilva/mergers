0c00c6a3f47574aaff999f2b5ee8ced6c4cce30d
Wadson Garbes <wadson.goncalves@fatec.sp.gov.br>
2018-07-24 16:29:16 -0300

    Atualiza run.py para servidores locais (e c9.io também)

--- run.py
+++ run.py
@@ -8,4 +8,8 @@ config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
+    app.run()
+    """
+    descomente a linha abaixo se estiver rodando no c9
+    """
+    # app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
